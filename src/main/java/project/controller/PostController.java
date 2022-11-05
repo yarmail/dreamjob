ea3dad5ede0e8764bf807b0e4748d8a1ba469331
yarmail <yarmail@yandex.ru>
2022-11-05 23:17:24 +0300

    4. Thymeleaf, Циклы. [#504841]

---
+++ src/main/java/project/controller/PostController.java
@@ -0,0 +1,26 @@
+package project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import project.storage.PostStore;
+
+/**
+ * Метод posts принимает объект Model.
+ * Он используется Thymeleaf для поиска объектов,
+ * которые нужны отобразить на виде.
+ * В Model мы добавляем объект posts
+ * Контроллер заполняет Model и передает два объекта в Thymeleaf –
+ * Model и View(posts.html). Thymeleaf генерирует HTML и возвращает ее клиенту.
+ */
+@Controller
+public class PostController {
+
+    private final PostStore postStore = PostStore.instOf();
+
+    @GetMapping ("/posts")
+    public String posts(Model model) {
+        model.addAttribute("posts", postStore.findAll());
+        return "posts";
+    }
+}
---
+++ src/main/java/project/storage/PostStore.java
@@ -0,0 +1,32 @@
+package project.storage;
+
+import project.model.Post;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Создадим хранилище PostStore. Оно будет синглтон
+ */
+public class PostStore {
+
+    private static final PostStore INST = new PostStore();
+
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    private PostStore() {
+        posts.put(1, new Post(1, "Junior Java Job"));
+        posts.put(2, new Post(1, "Middle Java Job"));
+        posts.put(3, new Post(1, "Senior Java Job"));
+    }
+
+    public static PostStore instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+
+}
