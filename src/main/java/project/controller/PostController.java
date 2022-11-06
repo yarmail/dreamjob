7ffbb3fb964e7c24ac363d37aa08b62aec238b99
yarmail <yarmail@yandex.ru>
2022-11-06 20:06:32 +0300

    7. HTML form. Создание вакансии. [#504844]

--- src/main/java/project/controller/PostController.java
+++ src/main/java/project/controller/PostController.java
@@ -3,25 +3,38 @@ package project.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import project.model.Post;
 import project.storage.PostStore;
 
-/**
- * Метод posts принимает объект Model.
- * Он используется Thymeleaf для поиска объектов,
- * которые нужны отобразить на виде.
- * В Model мы добавляем объект posts
- * Контроллер заполняет Model и передает два объекта в Thymeleaf –
- * Model и View(posts.html). Thymeleaf генерирует HTML и возвращает ее клиенту.
- *
- * Проверка: localhost:8080/posts
- */
+
 @Controller
 public class PostController {
     private final PostStore postStore = PostStore.instOf();
 
+    /**
+     * Метод posts принимает объект Model.
+     * Он используется Thymeleaf для поиска объектов,
+     * которые нужны отобразить на виде.
+     * В Model мы добавляем объект posts
+     * Контроллер заполняет Model и передает два объекта в Thymeleaf –
+     * Model и View(posts.html). Thymeleaf генерирует HTML и возвращает ее клиенту.
+     *
+     * Проверка: localhost:8080/posts
+     */
     @GetMapping ("/posts")
     public String posts(Model model) {
         model.addAttribute("posts", postStore.findAll());
         return "posts";
+    }
+
+    /**
+     *
+     * @param model
+     * @return
+     */
+    @GetMapping("/formAddPost")
+    public String addPost(Model model) {
+        model.addAttribute("post", new Post());
+        return "addPost";
     }
 }
